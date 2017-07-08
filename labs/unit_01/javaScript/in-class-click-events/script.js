d24c96f28133b7a7a04a5f6c7b73b11d38a3ecd7
anzcarroll <anzcarroll@gmail.com>
2017-07-07 20:08:05 -0400

    saving a bunch of classwork files that were touched on 7/7/17

--- labs/unit_01/javaScript/in-class-click-events/script.js
+++ labs/unit_01/javaScript/in-class-click-events/script.js
@@ -1,2 +1,21 @@
 window.onload = function() {
+var button1 = document.getElementById("button1");
+var button2 = document.getElementById('button2');
+var button3 = document.getElementById("button3");
+
+
+//need to crete function with all the elements that we need to click
+
+button1.addEventListener('click', function(event) {
+    alert('you have clicked a button');
+});
+
+button2.addEventListener('click', function(event) {
+    var newPTag = document.createElement('p');
+
+    p.innerHTML = A click event is essentially tying a function (as a callback) to an element and specifying what action needs to happen to trigger or call/invoke that function (callback).
+
 }
+}
+}
+
--- morning_exercises/unit_01/javaScript/making-change/index.js
+++ morning_exercises/unit_01/javaScript/making-change/index.js
@@ -6,7 +6,63 @@
 
 var makeChange = function(price, payment) {
   // Your Code Here
-};
+//  price =  price*100; 
+//  payment = payment*100;
+//  //wanting to not have whole dollars, getting decimals
+//  var change = Math.floor(payment-price);
+//  if (change <= 0){
+//    //returning no change if amount is 0
+//    return [0,0,0,0]
+
+//    //if we do have change, we want to go thru and divide how much chamge will be left for each Coin
+//    var Quarters = Math.floor(change/25);
+//    change = change%25;
+//    //im going to try and divide this x25 and give back whatever is left over here
+//    var Dimes = Math.floor(change/10);
+//    change = change%10;
+//    var nickels = Math.floor(change/5);
+//    change = change%5;
+//    var pennies = Math.floor(change);
+    
+//     var coins = [quarters, dimes, nickels, pennies]
+//     {
+//     return coins;
+//     }
+//  }
+
+
+  Your Code Here
+ //wanting to not have whole dollars, getting decimals
+ var change = Math.floor(payment*100)-(price*100);
+ if (change <= 0){
+   //returning no change if amount is 0
+   return [0,0,0,0]
+var result = [25,10,5,1].map(function(coins){
+  var numberOfCoins = Math.floor(change/coin);
+  change%=coin;
+  return numberOfCoins;
+})
+   //if we do have change, we want to go thru and divide how much chamge will be left for each Coin
+   var Quarters = Math.floor(change/25);
+   change %= 25;
+
+   //im going to try and divide this x25 and give back whatever is left over here
+   var Dimes = Math.floor(change/10);
+   change %= 10;
+
+   var nickels = Math.floor(change/5);
+   change %5;
+
+   var pennies = Math.floor(change);
+    
+    var coins = [quarters, dimes, nickels, pennies]
+
+    return coins;
+    }
+ }
+
+
+ 
 
 /// DO NOT EDIT BELOW THIS LINE ///
 module.exports = {
