699fd9c323d68b5e3b2345189f8cf565279902e5
anzcarroll <anzcarroll@gmail.com>
2017-07-11 08:29:22 -0400

    tried to work through homework some more..nothing wokring yet

--- homework/unit_01/javaScript/stopwatch/hw.js
+++ homework/unit_01/javaScript/stopwatch/hw.js
@@ -19,18 +19,41 @@ const Stopwatch = {
   // DO NOT EDIT ABOVE THIS LINE
   advanceTenMillisecs: function(){
     // Your Code Here
+    //  this.millisecs += 10;
+    // if (this.millisecs >= 1000) {
+    //   this.millisecs -= 1000;
+    //   this.secs++;
+    // }
+    // if (this.secs >= 60) {
+    //   this.secs -= 60;
+    //   this.mins++;
+    // }
   },
   reset: function(){
     // Your Code Here
+    this.mins = 0;
+    this.secs = 0;
+    this.millisecs = 0;
+    this.laps = 0;
+
   },
   start: function(){
     // Your Code Here
+    if (!(this.isRunning {
+      this.isRunning = true;
+    }
   },
   stop: function(){
     // Your Code Here
+    this.isRunning = false; 
+    }
   },
   lap: function(){
     // Your Code Here
+    // if (!this.isRunning) {
+    // laps.push('mins', 'secs', 'millisecs')
+       }
+    }
   }
 };
 
@@ -38,6 +61,10 @@ const Stopwatch = {
 const ViewEngine = {
   updateTimeDisplay: function(mins, secs, millisecs){
     // Your Code Here
+     //find the mins, secs, millisecs
+    $('#mins').html()
+    $('#secs').html()
+    $('millisecs').html()
   },
   updateLapListDisplay: function(laps){
     // Your Code Here
@@ -46,6 +73,11 @@ const ViewEngine = {
 const ViewHelpers = {
   zeroFill: function(number, length){
     // Your Code Here
+    //if # < digits.length
+    //return # as string 
+    // pad # with 0s until length ==== length;
+    //!(if # < digits.length
+    //return # as "" 
   },
 };
 
@@ -53,15 +85,33 @@ const ViewHelpers = {
 const AppController = {
   handleClockTick: function(){
     // Your Code Here
+    //find current time value from Stopwatch
+   //calls ViewEngine
+   //calls updateTimedisplay
+   ViewEngine.updateTimeDisplay(Stopwatch.mins, Stopwatch.secs, Stopwatch.millisecs);
+ }
+
   },
   handleClickStart: function() {
     // Your Code Here
+    if (Stopwatch.isRunning) { Stopwatch.start()};
   },
   handleClickStopReset: function(){
     // Your Code Here
+    if (Stopwatch.isRunning {
+      Stopwatch.stop();
+    }
+    if (!Stopwatch.isRunning) {
+      Stopwatch.reset();
+      //update time to 00:00:00
+      laps === [];
+
+    }
+      (mins, secs, millisecs)
   },
   handleClickLap: function(){
     // Your Code Here
+    ViewEngine.updateLapListDisplay
   }
 };
 
