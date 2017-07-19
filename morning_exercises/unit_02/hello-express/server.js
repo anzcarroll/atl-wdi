0ff8d559014d24de811bf878d29a7dde81f9fc38
anzcarroll <anzcarroll@gmail.com>
2017-07-19 13:22:56 -0400

    hello-express updated andd has psseudocode explanations

--- morning_exercises/unit_02/hello-express/server.js
+++ morning_exercises/unit_02/hello-express/server.js
@@ -1,15 +1,51 @@
+//pull in (any node package) to use in our file 
 const express = require('express');
+const hbs = require('hbs');
+//use express to run (constructor function) call it app because its application holding express function
 const app = express();
 
+//how we are going to tell express what engine to use to find templates
+ //it will automatically look for templates in the folder views-need to make views folder
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public')); // VERY IMPORTANT!! Make sure to add a '/'
+//__dirname says start from current dir , and allows us to set up relative paths 
+
+
+
+
+
+//only a get request when using .get
 app.get('/', (req, res) => {
     console.log(res);
-    res.send("hello wizardly world!!!");
+    res.send("hello wicked wizards!!");
     res.render('index.html');
 });
 
+//because hbs, it is going and looking in (rendering) greeting file, to pull back into our template
 app.get('/greeting', (req, res) => {
-res.send("hey cool, rad, awesome, coders of WDI11!!");
+    //render file called greeting
+    //
+    res.render('greetings', {
+      data: req.query.saying,
+      userName: req.query.name,
+      date: req.query.date,
+      place: req.query.location,
+    });
 });
+
+app.get('/favorite-foods', function(req, res) {
+    var favoriteFoods = ["olives from The Lawrence", 
+        'Cauliflower from YEAH!Burger', 
+        'Hops spicy chicken biscuit', 
+        'pistachio boba from Honey Bubble', 
+        'Pizza from Ammara'];
+
+    res.render('favorite_foods', {
+        data: favoriteFoods,
+    });
+});
+
 
 app.get('/rihanna', (req, res) => {
 res.send('work work work work work');
@@ -23,6 +59,8 @@ app.get("/name/:name",(req, res) => {
 }
 });
 
+//(/:'wildcard' paramater//url parameter)
+//code not clear of how many params it has on server side
 app.get('/users/:user_id/friends/:friend_id',(req, res) => {
 res.send(`hello, user #:${req.params.user_id}. Your friend is User#:${req.params.friend_id}.`)
 });
@@ -32,6 +70,9 @@ app.get('/delicious/:food', (req, res) => {
 res.send(`Man I love ${req.params.food}`);
 });
 
+//query params clear params bc on server side '?____=____&____=____
+//send us dynamic information
+//to make query param dont have to put param in param statement
 app.get('/sightings', (req,res) => {
     console.log(req.query);
 res.send(`how many ufo sighting do you think are in the state: ${req.query.state}? sights: ${req.query.sights}.`);
@@ -77,8 +118,9 @@ if (req.params.operator === "add") {
 });
 
 
-
+//if environment is set for port then use it, if not use 3000(default port)
 const port = process.env.PORT || 3000;
+//start listening on port(this will start the server)
 app.listen(port, () => {
 console.log("hello express is running" + port);
 });
