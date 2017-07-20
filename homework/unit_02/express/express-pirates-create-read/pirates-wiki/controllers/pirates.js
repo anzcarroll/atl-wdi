32a7768c58958bf21eab555950410410b80e8e26
anzcarroll <anzcarroll@gmail.com>
2017-07-20 19:52:12 -0400

    added almost everything, need to fix bug 500 for pirate/index view

--- homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
+++ homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
@@ -1,1 +1,44 @@
 //your code below
+var express = require("express");
+var router = express.Router();
+var pirates = require('../models/pirates.js');
+
+/*at the index of pirates*/
+router.get('/', (req, res) => {
+    res.render('pirates/index', {
+    pirates: pirates
+    });
+});
+
+
+/*new PIRATE*/
+router.get('/new', (req, res) => {
+    res.render('pirates/new');
+});
+
+router.get('/:id', (req, res) => {
+ const id = req.params.id;
+ const idPirate = pirates.seededPirates[id];
+res.render('pirates/show',{
+ pirate: idPirate
+});
+});
+
+router.post('/', (req, res) =>{
+const newPirate = {
+    name: req.body.name,
+    birthplace: req.body.birthplace,
+    deathYear: req.body.death_year,
+    base: req.body.base,
+    nickname: req.body.nickname,
+}
+pirate.seededPirates.push(newPirate);
+res.redirect('/pirates');
+});
+
+
+
+
+
+
+module.exports = router;
--- homework/unit_02/express/express-pirates-create-read/pirates-wiki/server.js
+++ homework/unit_02/express/express-pirates-create-read/pirates-wiki/server.js
@@ -1,1 +1,29 @@
 //your code here
+const express = require('express');
+const hbs = require('hbs');
+const bodyParser = require('body-parser');
+const pirateController = require('./controllers/pirates');
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+ app.use(bodyParser.json());
+
+app.set('view engine', 'hbs');
+
+/* HOME */
+app.get('/', function(req,res) {
+  res.send('This is our Home Page');
+});
+
+
+app.use("/pirates", pirateController);
+
+
+
+
+app.listen(port, () =>{
+    console.log("I'm going to make some pirates on port", port, "ARRRRGH");
+});
