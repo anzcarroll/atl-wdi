148180b717a0334ec9799d574b2df5252ec55f8d
anzcarroll <anzcarroll@gmail.com>
2017-07-26 21:03:54 -0400

    created donut schema

--- homework/unit_02/mongo/donut-shop/donut_mongoose/models/donuts.js
+++ homework/unit_02/mongo/donut-shop/donut_mongoose/models/donuts.js
@@ -1,7 +1,16 @@
 //requirements: require mongoose
+var express = require('express')
+const mongoose =require('mongoose');
 
 
 //create your donut schema:
-
+const newDonut = new mongoose.Schema ({
+    name: string,
+    description: string,
+    img: string,
+    price: number,
+    qty: number,
+});
 
 //export your donut with module.exports()
+module.exports = router;
