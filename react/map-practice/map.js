2197a387e70209b0b634dc3ddc210746963e02d7
anzcarroll <anzcarroll@gmail.com>
2017-08-17 12:09:07 -0400

    trying to fix conflicts

---
+++ react/map-practice/map.js
@@ -0,0 +1,27 @@
+// Uppercase each string in this array as a 
+//new array called uppercaseLyrics:
+const lyrics = ['never', 'gonna', 'give', 'you', 'up'];
+ const uppercaseLyrics = lyrics.map.ToUpperCase[i];
+
+
+
+
+ //Return an array of names for each of these person objects:
+
+ const people = [
+ 	{
+ 		name: 'George Michael',
+ 		age: 14,
+ 		title: 'Mr. Manager'
+ 	},
+ 	{
+ 		name: 'T-Bone',
+ 		age: 34,
+ 		title: 'Arsonist'
+ 	},
+ 	{
+ 		name: 'George Oscar',
+ 		age: 32,
+ 		title: 'Illusionist'
+ 	}
+ ];
