8a6d540ecc98a40b351467d228f3572f6a237532
anzcarroll <anzcarroll@gmail.com>
2017-08-18 08:54:19 -0400

    realty seed file created and db seeded

---
+++ homework/sql/realty/realty_seed.sql
@@ -0,0 +1,25 @@
+
+
+
+INSERT INTO apartments ( apt_num, bedrooms, bathrooms, address, tenant, occupied, sq_ft, price) 
+    VALUES ( '22',      1,       1,      '7 Bluebird Street, Seattle, WA', 'Helga', 'true', 700, 201021);
+INSERT INTO apartments ( apt_num, bedrooms, bathrooms, address, tenant, occupied, sq_ft, price) 
+    VALUES ( '03',      0,       1,      '2 RockaFella Pl, New York, NY', 'Beatrice', 'true', 500, 300500);
+INSERT INTO apartments ( apt_num, bedrooms, bathrooms, address, tenant, occupied, sq_ft, price) 
+    VALUES ( '12',      2,       2,      '563 Wet Willy Way, Orlando, FL', 'NA', 'false', 2100, 1111121);
+
+
+INSERT INTO offices ( office_num, floors, sq_ft, cubicles, bathrooms, address, name, occupied, price ) 
+    VALUES (300,40,20001,45,15,  '192 Finley Drive, Rochester, NY','Emporium Magorium',true,50000000);
+INSERT INTO offices ( office_num, floors, sq_ft, cubicles, bathrooms, address, name, occupied, price ) 
+    VALUES (150,5,3245,22,12,  '22 BallzBall Street, Dallas, GA' ,'Fliggin Fligger',true,2300000);
+INSERT INTO offices ( office_num, floors, sq_ft, cubicles, bathrooms, address, name, occupied, price ) 
+    VALUES (284,22,43431,56,9,  '7 Bigerly Ave, Austin, TX' ,'Kshlerin Group',true,999999);
+
+
+INSERT INTO storefronts ( address, occupied, price, kitchen, sq_ft, owner, outdoor_seating) 
+    VALUES ('22 No Way, Yessville, IL', 'true', 5000000, 'false', 2300, 'George', 'true');
+INSERT INTO storefronts ( address, occupied, price, kitchen, sq_ft, owner, outdoor_seating) 
+    VALUES ('467 Flenagin Street, Whooville, UT', 'false', 653000, 'true', 2300, 'Gilly', 'false');
+INSERT INTO storefronts ( address, occupied, price, kitchen, sq_ft, owner, outdoor_seating) 
+    VALUES ('544, Farmville, CA', 'true', 5000000, 'false', 2300, 'George', 'true');
