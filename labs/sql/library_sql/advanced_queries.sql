256c0a0c265d91630fd20e85ec6e143b119536a8
anzcarroll <anzcarroll@gmail.com>
2017-08-19 09:49:50 -0400

    added HW more_ruby file and worked thru advanced_queries in library sql

--- labs/sql/library_sql/advanced_queries.sql
+++ labs/sql/library_sql/advanced_queries.sql
@@ -2,15 +2,24 @@
 -- For all of these queries, you SHOULD NOT use an id # in a WHERE clause
 
 -- Find all fields (book and author related) for all books written by George R.R. Martin.
-SELECT authors.name, books.title FROM books JOIN authors ON authors.id = books.author_id WHERE authors.name='George R.R. Martin';
+SELECT * 
+FROM books 
+JOIN authors ON authors.id = books.author_id 
+WHERE UPPER(authors.name)=UPPER('George R.R. Martin');
 
 
 -- Find all fields (book and author related) for all books written by Milan Kundera.
-SELECT authors.name, books.title FROM books JOIN authors ON authors.id = books.author_id WHERE authors.name='Milan Kundera';
+SELECT *
+FROM books 
+JOIN authors ON authors.id = books.author_id 
+WHERE UPPER(authors.name)=UPPER('Milan Kundera');
 
 
 -- Find all books written by an author from China or the UK.
-SELECT authors.name, books.title FROM books JOIN authors ON authors.id = books.author_id WHERE (authors.nationality='China' OR authors.nationality='UK');
+SELECT * 
+FROM books 
+JOIN authors ON authors.id = books.author_id 
+WHERE (authors.nationality='China' OR authors.nationality='UK');
 
 
 -- Find out how many books Albert Camus wrote.
