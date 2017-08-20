b916bdb325fd5084f7cf150c6c0ec29c84479d12
anzcarroll <anzcarroll@gmail.com>
2017-08-20 14:18:34 -0400

    POODR Qs/As

--- homework/ruby/ruby-intro/3_More_Ruby/answers.sql
+++ homework/ruby/ruby-intro/3_More_Ruby/answers.sql
@@ -31,23 +31,45 @@ if you depend on classes that do too much
 
 
 6. What does `attr_reader` do?
-
+With instance variables, you can make them accessible 
+or not with attr_reader outside of the class.
+if you do this: 
+class Person
+  def initialize(name)
+    @name = name
+  end
+end
+you cant change thename of the person but if you use attr_reader you can:
+class Person
+  attr_reader :name
+  attr_writer :name
+  def initialize(name)
+    @name = name
+  end
+end
 
 
 7. What are 4 benefits Metz outlines for methods that have a single responsibility?
+1) expose previously hidden qualities
+2) avoid the need for comments
+3) encourages reuse
+4) easy to move to another class
 
-```
-Your Answer Here
-```
+
 
 8. What are 4 things an object knows when it has a dependency?
+1) the name of another class
+2) the name of a message that it intends to send to someone other than self
+3) the arguements that a message requires
+4) the order of those arguemnts 
 
-```
-Your Answer Here
-```
 
-9. Dependency management is core to creating future-proof applications. What does injecting dependencies and isolating dependencies do? What are their benefits?
+9. Dependency management is core to creating future-proof applications. 
 
-```
-Your Answer Here
-```
+What does injecting dependencies and isolating dependencies do? 
+injecting: Basically, instead of having your objects creating a dependency or asking a factory object to make one for them, you pass the needed dependencies in to the object externally, and you make it somebody elses problem. 
+isolationg:
+
+
+What are their benefits?
+dependency injection is that it can make testing lots easier
