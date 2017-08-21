6a9248f33cd9fdf6cf7c6594df9c750f2ef274e0
anzcarroll <anzcarroll@gmail.com>
2017-08-21 16:21:35 -0400

    sting skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -22,18 +22,34 @@
 
 # 3
 # Create a variable d and set it equal to a Fixnum
-d = 15
-# puts the value of d to the terminal
-puts d
-# Create a variable e and set it equal to a Float
-e = 2.222
-# puts the value of e to the terminal
-puts e
-# Set d equal to e on another line
-d = e 
-# puts the value of d to the terminal
-puts d
-# Commit -m "3. Integers Skills"
+# d = 15
+# # puts the value of d to the terminal
+# puts d
+# # Create a variable e and set it equal to a Float
+# e = 2.222
+# # puts the value of e to the terminal
+# puts e
+# # Set d equal to e on another line
+# d = e 
+# # puts the value of d to the terminal
+# puts d
+# # Commit -m "3. Integers Skills"
+
+# 4
+# Create a variable christine and set it equal to the string "Hi, I'm Christine"
+christine = "hi, I'm Christine"
+# Create a variable likesTo and set it equal to the String "long walks on the beach, kinda."
+likes_to = "long walks on the beach, kinda."
+# Using string interpolation and the variables tims and likesTo puts the following string to the terminal: "Hi, I'm Christine and I like long walks on the beach, kinda."
+puts " #{christine} and I like #{likes_to}"
+# Create a variable g and set it equal to the string "2"
+g = "2"
+# Create a variable gToNumber and set it equal to the Integer 2
+gToNumber = 2
+# puts the value of g + gToNumber in the terminal. What happens? How can you fix this?
+puts g + gToNumber # gives error
+puts "#{g} + #{gToNumber}" #gives '2+2'
+# Commit -m "4. String skills "
 
 
 
