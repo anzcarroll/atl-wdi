b4b4ee2a9cf2c68af3b29001eb6ccf66947fd522
anzcarroll <anzcarroll@gmail.com>
2017-08-21 16:16:32 -0400

    integer skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -1,3 +1,4 @@
+# 1
 #   a = true
 #   b = false
  
@@ -15,11 +16,24 @@
 
 
 
+# 2
+    # llamas = nil
+    # puts "#{llamas} + was here"
 
-    llamas = nil
-    puts "#{llamas} + was here"
-
-
+# 3
+# Create a variable d and set it equal to a Fixnum
+d = 15
+# puts the value of d to the terminal
+puts d
+# Create a variable e and set it equal to a Float
+e = 2.222
+# puts the value of e to the terminal
+puts e
+# Set d equal to e on another line
+d = e 
+# puts the value of d to the terminal
+puts d
+# Commit -m "3. Integers Skills"
 
 
 
