c48087bc605d2105209606c816862768ea05197d
anzcarroll <anzcarroll@gmail.com>
2017-08-21 16:11:34 -0400

    nil skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -1,26 +1,27 @@
-  a = true
-  b = false
+#   a = true
+#   b = false
  
-    if a 
-        puts "true is truthy, duh!"
-      else
-        puts "true is falsey, wtf!"
-      end
+#     if a 
+#         puts "true is truthy, duh!"
+#       else
+#         puts "true is falsey, wtf!"
+#       end
 
-      if b 
-        puts "true is truthy, duh!"
-      else
-        puts "true is falsey, wtf!"
-      end
+#       if b 
+#         puts "true is truthy, duh!"
+#       else
+#         puts "true is falsey, wtf!"
+#       end
 
 
-    
-# 5. :dart: Commit -m "1. Boolean Skills"
 
-# ## Challenge 2: Nil
-# 1.  Create a variable of your choosing and set it equal to Nil
-#     - p the value of the variable to the terminal
-#     - puts the value of the variable to the terminal concatenate the string "nil was here".
-#     - print the value of the variable to the terminal
-#     - puts the class of the value of the variable to the terminal.  Use the .class method.  Look up this method on Google.
-# 2. :dart: Commit -m "2. Nil skills."
+
+    llamas = nil
+    puts "#{llamas} + was here"
+
+
+
+
+
+
+
