7dce52001c6bcc29f0f976089847053045bdd97e
anzcarroll <anzcarroll@gmail.com>
2017-08-21 15:57:35 -0400

    class exercises and labs

---
+++ homework/ruby/ninja_skills.rb
@@ -0,0 +1,17 @@
+1.  a = true
+2.  b= false
+3.  Write a statement using `a` and `b` that evaluates to `false`
+    - puts the result to the terminal
+
+4.  Write a statement using `a` and `b` that evaluates to `true`
+    - puts the result to the terminal
+    
+5. :dart: Commit -m "1. Boolean Skills"
+
+## Challenge 2: Nil
+1.  Create a variable of your choosing and set it equal to Nil
+    - p the value of the variable to the terminal
+    - puts the value of the variable to the terminal concatenate the string "nil was here".
+    - print the value of the variable to the terminal
+    - puts the class of the value of the variable to the terminal.  Use the .class method.  Look up this method on Google.
+2. :dart: Commit -m "2. Nil skills."
