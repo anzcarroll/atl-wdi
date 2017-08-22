96d04e81517ea1e2278fa2a36817df85fc6a2bdc
anzcarroll <anzcarroll@gmail.com>
2017-08-22 08:26:33 -0400

    array skillsish and hashes skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -63,19 +63,58 @@
 
 # Create a small program that does the following
 # Asks the user for their name
-def grabName ()
-    puts "hello, what is your name?"
-    name = gets.to_s
-    puts "what would you like to do?"
-    to_do= gets.to_s
-    puts "#{name} would like to #{to_do}."
-    end
-    grabName()
+# def grabName ()
+#     puts "hello, what is your name?"
+#     name = gets.to_s
+#     puts "what would you like to do?"
+#     to_do= gets.to_s
+#     puts "#{name} would like to #{to_do}."
+#     end
+#     grabName()
 # Asks the user what they like to do
 # Prints out the user's name and what they like to do in a sentence.
 # Commit -m "5. Input/Output skills"
 
 
+# 6
+# # Create a variable myFirstArray and set it equal to an empty array
+# my_first_array = []
+# # Set the variable myFirstArray to an array with five Strings and four Integers
+# my_first_array = ["fish", 2, "food",9, 3, "fried", "plantain",5, "green"]
+# # Using myFirstArray puts the last value of the array to the terminal
+# puts my_first_array.last
+# # Using myFirstArray puts the first value of the array to the terminal
+# puts my_first_array.first
+# # Set the second value of myFirstArray to the String "Joe"
+# puts my_first_array.place("Joe").before("fish")
+# # Set the third value of myFirstArray to the Integer 3
+# puts my_first_array.
+# # Push the String Boolean true into myFirstArray
+# puts my_first_array.push("true")
+# # Puts the class of the fourth value in myFirstArray
+# puts my_first_array.
+# # Commit -m "6. Arrays skills"
+
+
+# 7
+# Create a variable called myFavoriteAnimals and set it equal to a Hash with five animals. 
+
+ my_fav_animals = { 
+     "Edgar" => "Donkey", 
+     "Jolene" => "Horse", 
+     "Fifi"  => "Cat", 
+     "Hussun" => "Husky", 
+     "Rascal" => "Hedgehog" }
+     
+# Change value of Edgar to be "Bear"
+my_fav_animals['Edgar'] = "Bear"
+puts my_fav_animals
+# Create a variable favoriteMovie and set it equal to a new, empty Hash.
+my_fav_movie = {}
+# Edit the favoriteMovie hash and add a movie key with a value of your choosing.
+my_fav_movie['pretty_woman'] = true
+puts my_fav_movie
+# Commit -m "7. Hashes skills"
 
 
 
